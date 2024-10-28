12d7a98843f0127dfd574eaa0c24bb91e0a19612
Marcelo Saldanha <marcelo_saldanha5112@hotmail.com>
2024-10-28 01:40:18 -0300

    change urls

--- api_getatende/urls.py
+++ api_getatende/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('', admin.site.urls),
+    path('', RedirectView.as_view(url='/painel/')),
+    path('painel', admin.site.urls),
     path('maquina-obsoleta/', include('maquina_obsoleta.urls'), name='maquina_obsoleta'),
     path('ecommerce/', include('ecommerce.urls'), name='e-commerce'),
 ]
