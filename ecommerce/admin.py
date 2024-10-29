94ed6272ce058b63665982c60fcea27af96dffa9
Marcelo Saldanha <marcelo_saldanha5112@hotmail.com>
2024-10-28 23:59:59 -0300

    Include export to CSV

--- ecommerce/admin.py
+++ ecommerce/admin.py
@@ -1,5 +1,22 @@
+import csv
 from django.contrib import admin
-
+from django.http import HttpResponse
 from .models import CnpjEcommerce
 
-admin.site.register(CnpjEcommerce)
+@admin.register(CnpjEcommerce)
+class CnpjAdmin(admin.ModelAdmin):
+    list_display = ('id_cnpj','number_cnpj',)
+    search_fields = ('number_cnpj',)
+    ordering = ('id_cnpj',)
+    
+    def export_to_csv(self, request, queryset):
+        response = HttpResponse(content_type='text/csv')
+        response ['Content-Disposition' ] = 'attachment; filename="CNPJs.csv"'
+        writer = csv.writer(response)
+        writer.writerow(['id', 'CNPJ'])
+        for cnpj in queryset:
+            writer.writerow([cnpj.id_cnpj, cnpj.number_cnpj])
+        return response
+
+    export_to_csv.short_description = 'Exportar para CSV'
+    actions = [export_to_csv]
--- maquina_obsoleta/admin.py
+++ maquina_obsoleta/admin.py
@@ -1,5 +1,23 @@
+import csv
 from django.contrib import admin
-
+from django.http import HttpResponse
 from .models import SerialNumber
 
-admin.site.register(SerialNumber)
+
+@admin.register(SerialNumber)
+class SerialAdmin(admin.ModelAdmin):
+    list_display = ('id_serial','number_serial',)
+    search_fields = ('number_serial',)
+    ordering = ('id_serial',)
+    
+    def export_to_csv(self, request, queryset):
+        response = HttpResponse(content_type='text/csv')
+        response ['Content-Disposition' ] = 'attachment; filename="Seriais.csv"'
+        writer = csv.writer(response)
+        writer.writerow(['id', 'serial'])
+        for serial in queryset:
+            writer.writerow([serial.id_serial, serial.number_serial])
+        return response
+
+    export_to_csv.short_description = 'Exportar para CSV'
+    actions = [export_to_csv]
